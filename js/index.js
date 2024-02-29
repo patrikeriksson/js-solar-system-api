0639518be052af69e961e3152abeb9966f451152
Patrik Eriksson <puttrik96@hotmail.com>
2024-02-29 21:38:05 +0100

    Heading now changes to planet name if you hover a planet and added some comments.

--- js/index.js
+++ js/index.js
@@ -26,3 +26,75 @@ searchForm.addEventListener("submit", async (e) => {
     alert(`${searchInput.value} är ingen planet`);
   }
 });
+
+// Changes the main- and sub-heading to show the planets name and latin name while hovering, then changes back on mouse-out
+const mainHeading = document.querySelector(".main-heading");
+const subHeading = document.querySelector(".sub-heading");
+
+const sun = document.querySelector(".sun");
+sun.addEventListener("mouseover", () => {
+  mainHeading.textContent = "SOLEN";
+  subHeading.textContent = "SOLIS";
+});
+sun.addEventListener("mouseout", handleMouseOut);
+
+const mercury = document.querySelector(".mercury");
+mercury.addEventListener("mouseover", () => {
+  mainHeading.textContent = "MERKURIUS";
+  subHeading.textContent = "MERCURIALIS";
+});
+mercury.addEventListener("mouseout", handleMouseOut);
+
+const venus = document.querySelector(".venus");
+venus.addEventListener("mouseover", () => {
+  mainHeading.textContent = "VENUS";
+  subHeading.textContent = "VENUS";
+});
+venus.addEventListener("mouseout", handleMouseOut);
+
+const earth = document.querySelector(".earth");
+earth.addEventListener("mouseover", () => {
+  mainHeading.textContent = "JORDEN";
+  subHeading.textContent = "TELLUS";
+});
+earth.addEventListener("mouseout", handleMouseOut);
+
+const mars = document.querySelector(".mars");
+mars.addEventListener("mouseover", () => {
+  mainHeading.textContent = "MARS";
+  subHeading.textContent = "MARS";
+});
+mars.addEventListener("mouseout", handleMouseOut);
+
+const jupiter = document.querySelector(".jupiter");
+jupiter.addEventListener("mouseover", () => {
+  mainHeading.textContent = "JUPITER";
+  subHeading.textContent = "LOVIS";
+});
+jupiter.addEventListener("mouseout", handleMouseOut);
+
+const saturn = document.querySelector(".saturn");
+saturn.addEventListener("mouseover", () => {
+  mainHeading.textContent = "SATURNUS";
+  subHeading.textContent = "SATURNUS";
+});
+saturn.addEventListener("mouseout", handleMouseOut);
+
+const uranus = document.querySelector(".uranus");
+uranus.addEventListener("mouseover", () => {
+  mainHeading.textContent = "URANUS";
+  subHeading.textContent = "URANUS";
+});
+uranus.addEventListener("mouseout", handleMouseOut);
+
+const neptune = document.querySelector(".neptune");
+neptune.addEventListener("mouseover", () => {
+  mainHeading.textContent = "NEPTUNUS";
+  subHeading.textContent = "NEPTUNUS";
+});
+neptune.addEventListener("mouseout", handleMouseOut);
+
+function handleMouseOut() {
+  mainHeading.textContent = "SOLSYSTEMET";
+  subHeading.textContent = "SOLARIS";
+}
--- js/planet.js
+++ js/planet.js
@@ -47,38 +47,45 @@ async function getPlanet() {
 
   // Selects the element with the planet-info class and adds new html elements containing the data from the API
   document.querySelector(".planet-info").innerHTML = `
-          <h1 class="main-heading">${planet.name}</h1>
-          <h2 class="sub-heading">${planet.latinName}</h2>
-          <section>
-            <p class="planet-content">${planet.desc}</p>
-          </section>
-          <hr>
-          <section class="planet-specs">
-            <div>
-              <h3 class="planet-sub-heading">OMKRETS</h3>
-              <p class="planet-content">${planet.circumference} km</p>
-            </div>
-            <div>
-              <h3 class="planet-sub-heading">KM FRÅN SOLEN</h3>
-              <p class="planet-content">${planet.distance} km</p>
-            </div>
-            <div>
-              <h3 class="planet-sub-heading">MAX TEMPERATUR</h3>
-              <p class="planet-content">${planet.temp.day}&deg;C</p>
-            </div>
-            <div>
-              <h3 class="planet-sub-heading">MIN TEMPERATUR</h3>
-              <p class="planet-content">${planet.temp.night}&deg;C</p>
-            </div>
-          </section>
-          <hr>
-          <section>
-            <h3 class="planet-sub-heading">MÅNAR</h3>
-            <p class="planet-content">${
-              planet.moons.length > 0 ? planet.moons.join(", ") : "Saknar månar"
-            } </p>
-          </section>
-        `;
+    <h1 class="main-heading">${planet.name}</h1>
+    <h2 class="sub-heading">${planet.latinName}</h2>
+    <section>
+      <p class="planet-content">${planet.desc}</p>
+    </section>
+    <hr>
+    <section class="planet-specs">
+      <div>
+        <h3 class="planet-sub-heading">OMKRETS</h3>
+        <p class="planet-content">${formatThousands(
+          planet.circumference
+        )} km</p>
+      </div>
+      <div>
+        <h3 class="planet-sub-heading">KM FRÅN SOLEN</h3>
+        <p class="planet-content">${formatThousands(planet.distance)} km</p>
+      </div>
+      <div>
+        <h3 class="planet-sub-heading">MAX TEMPERATUR</h3>
+        <p class="planet-content">${planet.temp.day}&deg;C</p>
+      </div>
+      <div>
+        <h3 class="planet-sub-heading">MIN TEMPERATUR</h3>
+        <p class="planet-content">${planet.temp.night}&deg;C</p>
+      </div>
+    </section>
+    <hr>
+    <section>
+      <h3 class="planet-sub-heading">MÅNAR</h3>
+      <p class="planet-content">${
+        planet.moons.length > 0 ? planet.moons.join(", ") : "Saknar månar"
+      } </p>
+    </section>
+  `;
+}
+
+// Makes the numbers more readable
+function formatThousands(number) {
+  return number.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, " ");
 }
 
 getPlanet();
